0a73017aa5ed0b57af1f5b42630416936cf7f7f4
agile <toptal.powerful@gmail.com>
2023-08-13 22:48:39 +0530

    style:add shimmer to the RestaurantCard

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,13 +5,13 @@ const Loading = () => {
   return (
     <main>
       <Header />
-      <div className="py-3 mt-10 flex flex-wrap  justify-center max-w-6xl m-auto">
+      <div className="bg-gray-100 py-3 mt-10 max-w-6xl sm:w-full m-auto flex flex-wrap justify-center">
         {Array(12)
           .fill("")
           .map((_, index) => (
             <div
               key={index}
-              className="animate-pulse bg-white w-64 sm:w-80 h-78 m-3 overflow-hidden cursor-pointer"
+              className="animate-pulse bg-white w-64 sm:w-80 h-72 m-3 overflow-hidden"
             ></div>
           ))}
       </div>
